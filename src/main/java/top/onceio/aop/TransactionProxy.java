94d67ad684ff0d7ab886be69018b2d2a075ccf21
maxian <Ma@danglaoshi.net>
2018-02-03 18:55:10 +0800

    TODO Cache

--- src/main/java/top/onceio/aop/TransactionProxy.java
+++ src/main/java/top/onceio/aop/TransactionProxy.java
@@ -6,6 +6,10 @@ import net.sf.cglib.proxy.MethodInterceptor;
 import net.sf.cglib.proxy.MethodProxy;
 import top.onceio.aop.annotation.Transactional;
 import top.onceio.beans.BeansEden;
+import top.onceio.cache.Cache;
+import top.onceio.cache.annotation.CacheEvict;
+import top.onceio.cache.annotation.CachePut;
+import top.onceio.cache.annotation.Cacheable;
 import top.onceio.db.jdbc.JdbcHelper;
 
 public class TransactionProxy implements MethodInterceptor {
@@ -29,6 +33,26 @@ public class TransactionProxy implements MethodInterceptor {
 			}
 		} else {
 			result = proxy.invokeSuper(obj, args);
+		}
+		return result;
+	}
+	//TODO
+	public Object get(Object obj,Method method,Object[] args) {
+		Object result = null;
+		Cache cache = BeansEden.get().load(Cache.class);
+		if(cache != null) {
+			String key = null;
+			Object ret = null;
+			CacheEvict evict = method.getAnnotation(CacheEvict.class);
+			CachePut put = method.getAnnotation(CachePut.class);
+			Cacheable cacheable = method.getAnnotation(Cacheable.class);
+			if(cacheable != null) {
+				result = cache.get(key, method.getReturnType());
+			} else if(evict != null) {
+				cache.evict(key);
+			}else if(put != null) {
+				cache.put(key, ret);
+			}
 		}
 		return result;
 	}
